babad9c101219574aaee70a5acb8e177ceb68412
AMYNA-7 <amyniane591@gmail.com>
2019-05-10 14:46:28 +0200

    projet d' allumage de lampe avec java

---
+++ laampe/java.js
@@ -0,0 +1,10 @@
+
+function bulb() {
+	document.getElementById("eteindre").src="/home/amyniane/Bureau/laampe/on.jpg"
+ 	
+}
+//fonction pour allumer
+function bulboff() {
+	document.getElementById("eteindre").src="/home/amyniane/Bureau/laampe/off.jpg"
+}
+ 	
